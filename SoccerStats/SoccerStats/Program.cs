eeaae33ef706d47b4e87319f2be6d90042e1827f
rainbow82 <shannonbeach1982@gmail.com>
2016-08-21 14:34:21 -0400

    Reading the data file for input

---
+++ SoccerStats/SoccerStats/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace SoccerStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+
+            var fileName = Path.Combine(directory.FullName, "data.txt");
+            var file = new FileInfo(fileName);
+            if (file.Exists)
+            {
+                //using = making sure the data file closes
+                using (var reader = new StreamReader(file.FullName))
+                {
+                    Console.SetIn(reader);
+                    Console.WriteLine(Console.ReadLine());
+                }
+                    
+            }
+           
+            
+
+        }// end main
+    }//end program
+}// end namespace
