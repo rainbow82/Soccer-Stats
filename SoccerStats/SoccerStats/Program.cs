fd7257021cdd9a74a41cfc5d4eafc847a324a55f
rainbow82 <shannonbeach1982@gmail.com>
2016-09-01 21:41:26 -0400

    Sorting player list to top ten players

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -20,10 +20,10 @@ namespace SoccerStats
             var fileContents = ReadSoccerResults(fileName);
             fileName = Path.Combine(directory.FullName, "players.json");
             var players = DeserializePlayers(fileName);
-
-            foreach (var player in players)
+            var topTenPlayers = GetTopTenPlayers(players);
+            foreach (var player in topTenPlayers)
             {
-                Console.WriteLine(player.FirstName);
+                Console.WriteLine("Name: " + player.FirstName + "PPG: " + player.PointsPerGame);
             }
         }// end main
 
@@ -98,6 +98,23 @@ namespace SoccerStats
             }
                 
             return players;
+        }
+
+        public static List<Player> GetTopTenPlayers(List<Player> players)
+        {
+            var topTenPlayers = new List<Player>();
+            players.Sort(new PlayerComparer());
+            int counter = 0;
+            foreach (var player in players)
+            {
+                topTenPlayers.Add(player);
+                counter++;
+                if (counter == 10)
+                {
+                    break;
+                }
+            }
+            return topTenPlayers;
         }
     }//end program
 }// end namespace
